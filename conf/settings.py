9bcdfe84820d20e53815e5639e05919aed2f85a6
Nathan Little <nthnlttl@gmail.com>
2021-11-01 11:48:25 -0400

    heroku deployment

--- conf/settings.py
+++ conf/settings.py
@@ -24,7 +24,10 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1:8000",
+    "final-project-ccs-nthnlttl.herokuapp.com"
+]
 
 
 # Application definition
