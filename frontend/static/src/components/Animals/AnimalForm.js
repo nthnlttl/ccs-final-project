136c3a14ad03fbb9b508860068559854b8ba59be
Nathan Little <nthnlttl@gmail.com>
2021-11-10 09:47:13 -0500

    making sure app still works on heroku

--- frontend/static/src/components/Animals/AnimalForm.js
+++ frontend/static/src/components/Animals/AnimalForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Cookies from 'js-cookie';
+
 
 export default function Animals(props) {
     const [animal, setAnimal] = useState({
@@ -14,10 +14,68 @@ export default function Animals(props) {
         good_with_children: null=false,
         house_trained: null=false,
         picture: null=true,
-    })
+    });
+
+    function handleInput(event) {
+        const {name, value} = event.target;
+        setAnimal(prevState => ({
+            ...prevState,
+            [name]: value,
+        }));
+    }
+
+    function handleError(error) {
+        console.warn(error);
+    }
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json',
+            },
+            body: JSON.stringify(animal)
+        };
+
+        const response = await fetch('/api_v1/animals/', options).catch(handleError);
+        if(!response) {
+            console.log(response);
+        } else {
+            const data = await response.json();
+            setAnimal((prevState) => ({
+                ...prevState
+            }))
+        }
+    }
     return (
-        <div>
-            
-        </div>
+        <form className='mt-3 col-6' onSubmit={handleSubmit}>
+            <div className='form-group text-left mb-3'>
+                <label htmlFor='name'>Name</label>
+                <input
+                    type='text'
+                    className='form-control'
+                    id='name'
+                    placeholder='Enter Pet Name.'
+                    onChange={handleInput}
+                    required
+                    name='name'
+                    value={animal.animal}
+                />
+                <div className='form-group text-left mb-3'>
+                <label htmlFor='name'>Name</label>
+                <input
+                    type=
+                    className='form-control'
+                    placeholder=
+                    onChange={handleInput}
+                    required
+                    name=
+                    value=
+                />
+            </div>
+            </div>
+        </form>
     )
 }
